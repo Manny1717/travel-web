b6a4009fa870ec839cce9eb8db19ff60b5a51e84
Manuel Reyes <Reyesmanuel959@yahoo.com>
2024-04-22 18:09:16 -0700

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { BsPerson } from "react-icons/bs";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaPinterest,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+{
+  /*Function for drop down menu */
+}
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [logo, setLogo] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    setLogo(!logo);
+  };
+
+  return (
+    <div className="flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white">
+      <div>
+        <h1 onClick={handleNav} className={logo ? "hidden" : "block"}>
+          BEACHES.
+        </h1>
+      </div>
+      {/*UL and Icons will be hidden until the screen reaches medium screen.*/}
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>Destinations</li>
+        <li>Travel</li>
+        <li>View</li>
+        <li>Book</li>
+      </ul>
+
+      <div className="hidden md:flex">
+        <BsPerson className="mr-2" size={20} />
+        <BiSearch size={20} />
+      </div>
+
+      {/* HAMBURGER MENU */}
+      <div onClick={handleNav} className="md:hidden z-10">
+        {nav ? (
+          <AiOutlineClose className="text-black" size={20} />
+        ) : (
+          <HiOutlineMenuAlt4 size={20} />
+        )}
+      </div>
+
+      {/* MOBILE MENU DROPDOWN
+          This is the customaztion of the drop down menu. */}
+      <div
+        onClick={handleNav}
+        className={
+          nav
+            ? "absolute left-0 top-0 w-full text-black bg-gray-100/90 px-4 py-7 flex flex-col"
+            : "absolute left-[-100%]"
+        }
+      >
+        <ul>
+          <h1>BEACHES.</h1>
+          <li className="border-b">Home</li>
+          <li className="border-b">Destinations</li>
+          <li className="border-b">Travel</li>
+          <li className="border-b">View</li>
+          <li className="border-b">Book</li>
+          <div className="flex flex-col">
+            <button className="my-6">Search</button>
+            <button>Account</button>
+          </div>
+          <div className="flex justify-between my-6">
+            <FaFacebook className="icon" />
+            <FaTwitter className="icon" />
+            <FaInstagram className="icon" />
+            <FaPinterest className="icon" />
+            <FaYoutube className="icon" />
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
